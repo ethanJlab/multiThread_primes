b55f7bb970af24848ef9d2b522fbe41367adc720
ethanjanicki <ethanjanicki89@knights.ucf.edu>
2023-01-18 01:26:27 -0500

    HBJH

--- primeThreading.cpp
+++ primeThreading.cpp
@@ -8,41 +8,41 @@ using namespace std::chrono;
 
 std::mutex mtx;
 
-const long upperBound = 1000000 + 1;
-const long sqrtOfUpperBound = 1000 + 1;
+const long long upperBound = 100 + 1;
+const long long sqrtOfUpperBound = 10 + 1;
 
-long getTicket(long ticketArray[sqrtOfUpperBound]);
-void checkOffNumber(long num, long ticketArray[sqrtOfUpperBound], map <long,bool> &isMultiple);
-void findMultiples(long num, map <long,bool> &isMultiple,long ticketArray[sqrtOfUpperBound]);
-void fillMap(map <long,bool> &isMultiple);
-void checkOffTicket (long num, long ticketArray[sqrtOfUpperBound]);
-void findPrimes(long startingNum, map <long,bool> &isMultiple, long ticketArray[sqrtOfUpperBound]);
+long long getTicket(long long ticketArray[sqrtOfUpperBound]);
+void checkOffNumber(long long num, long long ticketArray[sqrtOfUpperBound], map <long long,bool> &isMultiple);
+void findMultiples(long long num, map <long long,bool> &isMultiple,long long ticketArray[sqrtOfUpperBound]);
+void fillMap(map <long long,bool> &isMultiple);
+void checkOffTicket (long long num, long long ticketArray[sqrtOfUpperBound]);
+void findPrimes(long long startingNum, map <long long,bool> &isMultiple, long long ticketArray[sqrtOfUpperBound]);
 
 int main()
 {
     // start timer
     auto start = high_resolution_clock::now();
 
-    long ticketArray[sqrtOfUpperBound];
-    map <long,bool> isMultiple;
+    long long ticketArray[sqrtOfUpperBound];
+    map <long long,bool> isMultiple;
 
-    long sum;
+    long long sum;
     // sum of all the number 1 to upperBound
     sum = ((upperBound - 1) * upperBound / 2) - 1;
-    long primeCount = upperBound - 2;
+    long long primeCount = upperBound - 2;
     
     // fill the ticketArray
     ticketArray[0] = 0;
     ticketArray[1] = 0;
     
-    for (long i = 2; i < sqrtOfUpperBound; i++) {
+    for (long long i = 2; i < sqrtOfUpperBound; i++) {
         ticketArray[i] = i;
     }
 
    
     // find all primes
     /*
-    for (long i = 2; i < sqrtOfUpperBound; i++) {
+    for (long long i = 2; i < sqrtOfUpperBound; i++) {
         if (isMultiple[i] != true) {
             findMultiples(i, isMultiple, ticketArray);
         }
@@ -51,7 +51,6 @@ int main()
     
     
     // get ticket and find multiples
-    // TODO, add starting numbers coresponding to the first 8 primes
     
     //findPrimes(isMultiple, ticketArray);
     //create 8 threads and run findPrimes on each
@@ -59,23 +58,23 @@ int main()
     thread t2(findPrimes, 3, ref(isMultiple), ticketArray);
     thread t3(findPrimes, 5, ref(isMultiple), ticketArray);
     thread t4(findPrimes, 7, ref(isMultiple), ticketArray);
-    thread t5(findPrimes, 11, ref(isMultiple), ticketArray);
-    thread t6(findPrimes, 13, ref(isMultiple), ticketArray);
-    thread t7(findPrimes, 17, ref(isMultiple), ticketArray);
-    thread t8(findPrimes, 19, ref(isMultiple), ticketArray);
+    //thread t5(findPrimes, 11, ref(isMultiple), ticketArray);
+    //thread t6(findPrimes, 13, ref(isMultiple), ticketArray);
+    //thread t7(findPrimes, 17, ref(isMultiple), ticketArray);
+    //thread t8(findPrimes, 19, ref(isMultiple), ticketArray);
 
     t1.join();
     t2.join();
     t3.join();
     t4.join();
-    t5.join();
-    t6.join();
-    t7.join();
-    t8.join();
+    //t5.join();
+    //t6.join();
+    //t7.join();
+    //t8.join();
 
-    // prlong all primes
+    // prlong long all primes
     
-    for (long i = 2; i < upperBound; i++) {
+    for (long long i = 2; i < upperBound; i++) {
         if (isMultiple[i] == true) {
             //cout << i << endl;
             sum -= i;
@@ -86,14 +85,14 @@ int main()
     cout << "Sum of primes: " << sum << endl;
     cout << "Number of primes: " << primeCount << endl;
 
-    /*for (long i = 250000; true; i--) {
+    /*for (long long i = 250000; true; i--) {
         if ((i) * (sizeof(bool)) < 65536) {
             cout << "Magic number: " << i << endl;
             break;
         }
     }
     */
-    // end timer and prlong time
+    // end timer and prlong long time
     auto stop = high_resolution_clock::now();
     auto duration = duration_cast<microseconds>(stop - start);
     cout << "Time taken: " << endl;
@@ -105,9 +104,9 @@ int main()
     return 0;
 }
 
-long getTicket(long ticketArray[sqrtOfUpperBound]) {
+long long getTicket(long long ticketArray[sqrtOfUpperBound]) {
     // goes through the ticket array and returns the first non-zero number
-    for (long i = 0; i < sqrtOfUpperBound; i++) {
+    for (long long i = 0; i < sqrtOfUpperBound; i++) {
         if (ticketArray[i] != 0) {
             return ticketArray[i];
         }
@@ -115,10 +114,10 @@ long getTicket(long ticketArray[sqrtOfUpperBound]) {
     return 0;
 }
 
-void checkOffNumber(long num, long ticketArray[sqrtOfUpperBound], map <long,bool> &isMultiple) {
+void checkOffNumber(long long num, long long ticketArray[sqrtOfUpperBound], map <long long,bool> &isMultiple) {
     
     // find number in ticketArray and set it to 0
-    for (long i = 0; i < sqrtOfUpperBound; i++) {
+    for (long long i = 0; i < sqrtOfUpperBound; i++) {
         if (ticketArray[i] == num) {
             mtx.lock();
             ticketArray[i] = 0;
@@ -132,8 +131,8 @@ void checkOffNumber(long num, long ticketArray[sqrtOfUpperBound], map <long,bool
     
 }
 
-void checkOffTicket (long num, long ticketArray[sqrtOfUpperBound]) {
-    for (long i = 0; i < sqrtOfUpperBound; i++) {
+void checkOffTicket (long long num, long long ticketArray[sqrtOfUpperBound]) {
+    for (long long i = 0; i < sqrtOfUpperBound; i++) {
         if (ticketArray[i] == num) {
             mtx.lock();
             ticketArray[i] = 0;
@@ -143,25 +142,25 @@ void checkOffTicket (long num, long ticketArray[sqrtOfUpperBound]) {
     }
 } 
 
-void findMultiples(long num, map <long,bool> &isMultiple,long ticketArray[sqrtOfUpperBound]) {
-    long currentNum = num * 2;
+void findMultiples(long long num, map <long long,bool> &isMultiple,long long ticketArray[sqrtOfUpperBound]) {
+    long long currentNum = num * 2;
    
     checkOffTicket(num, ticketArray); 
     
-    for (long i = currentNum; i < upperBound; i += num){
+    for (long long i = currentNum; i < upperBound; i += num){
         checkOffNumber(i, ticketArray, isMultiple);
     }          
     
 }
 
-void fillMap(map <long,bool> &isMultiple) {
-    for (long i = 0; i < upperBound; i++) {
+void fillMap(map <long long,bool> &isMultiple) {
+    for (long long i = 0; i < upperBound; i++) {
         isMultiple[i] = true;
     }
 }
 
-void findPrimes(long startingNum, map <long,bool> &isMultiple, long ticketArray[sqrtOfUpperBound]) {
-    long ticket = startingNum;
+void findPrimes(long long startingNum, map <long long,bool> &isMultiple, long long ticketArray[sqrtOfUpperBound]) {
+    long long ticket = startingNum;
     while (ticket != 0) {
         if (isMultiple[ticket] != true) {
             findMultiples(ticket, isMultiple, ticketArray);
---
+++ sieve.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <chrono>
+#include <vector>
+using namespace std;
+using namespace std::chrono;
+
+std::mutex mtx;
+
+const long long upperBound = 100000000 + 1;
+const long long sqrtOfUpperBound = 10000 + 1;
+
+void checkNonPrimes(long long start, vector <bool> &primeList);
+int getNextNumber(vector <bool> &ticketArray);
+void findAllPrimes(vector <bool> &primeList, vector <bool> &ticketArray, int start);
+
+int main() {
+    // start timer
+    auto start = high_resolution_clock::now();
+
+    vector <bool> primeList(upperBound, true);
+    //false if not yet used (or dont need to use)
+    //true if used
+    vector <bool> ticketArray(sqrtOfUpperBound, false);
+
+    primeList[0] = false;
+    primeList[1] = false;
+    ticketArray[0] = true;
+    ticketArray[1] = true;    
+
+    thread t1(findAllPrimes, ref(primeList), ref(ticketArray), 2);
+    thread t2(findAllPrimes, ref(primeList), ref(ticketArray), 3);
+    thread t3(findAllPrimes, ref(primeList), ref(ticketArray), 5);
+    thread t4(findAllPrimes, ref(primeList), ref(ticketArray), 7);
+    thread t5(findAllPrimes, ref(primeList), ref(ticketArray), 11);
+    thread t6(findAllPrimes, ref(primeList), ref(ticketArray), 13);
+    thread t7(findAllPrimes, ref(primeList), ref(ticketArray), 17);
+    thread t8(findAllPrimes, ref(primeList), ref(ticketArray), 19);
+
+    t1.join();
+    t2.join();
+    t3.join();
+    t4.join();
+    t5.join();
+    t6.join();
+    t7.join();
+    t8.join();
+
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start);
+
+    
+    long long sum = 0;
+    long long numPrimes = 0;
+
+    for (long long i = 0; i < upperBound; i++) {
+        if (primeList[i] == true) {
+            sum += i;
+            numPrimes++;
+        }
+    }
+
+    cout << "Sum of all primes: " << sum << endl;
+    cout << "Number of primes: " << numPrimes << endl;
+
+    // end timer
+    cout << "Microseconds: " << duration.count() << " microseconds" << endl;
+    cout << "Seconds: " << duration.count() / 1000000.0 << " seconds" << endl;
+
+    return 0;
+
+}
+
+void checkNonPrimes(long long start, vector <bool> &primeList) {
+    for (long long i = start * start; i < upperBound; i += start) {
+        if (primeList[i] == false) {
+            continue;
+        }
+        mtx.lock();
+        primeList[i] = false;
+        mtx.unlock();
+    }
+}
+
+int getNextNumber(vector <bool> &ticketArray) {
+    for (int i = 2; i < sqrtOfUpperBound; i++) {
+        if (ticketArray[i] == false) {
+            mtx.lock();
+            if (ticketArray[i] == true) {
+                mtx.unlock();
+                continue;
+            }
+            ticketArray[i] = true;
+            mtx.unlock();
+            return i;
+        }
+    }
+    return -1;
+}
+
+void findAllPrimes(vector <bool> &primeList, vector <bool> &ticketArray, int start) {
+    int nextNumber = start;
+    while (nextNumber != -1) {
+        checkNonPrimes(nextNumber, primeList);
+        nextNumber = getNextNumber(ticketArray);
+    }
+}
+    
+
+    
