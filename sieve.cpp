b3ebbea66638c86c2db42b1b83da0b3a411b802a
ethanjanicki <ethanjanicki89@knights.ucf.edu>
2023-01-24 18:17:15 -0500

    working

--- sieve.cpp
+++ sieve.cpp
@@ -10,14 +10,17 @@ std::mutex mtx;
 
 const long long upperBound = 100000000 + 1;
 const long long sqrtOfUpperBound = 10000 + 1;
+long long sum = 2;
+long long numPrimes = 1;
 
 void checkNonPrimes(long long start, vector <bool> &primeList);
-int getNextNumber(vector <bool> &ticketArray);
+int getNextNumber(vector <bool> &ticketArray, int start, vector <bool> &primeList);
 void findAllPrimes(vector <bool> &primeList, vector <bool> &ticketArray, int start);
 
 int main() {
     // start timer
     auto start = high_resolution_clock::now();
+    cout << "Program started" << endl;
 
     vector <bool> primeList(upperBound, true);
     //false if not yet used (or dont need to use)
@@ -29,32 +32,31 @@ int main() {
     ticketArray[0] = true;
     ticketArray[1] = true;    
 
-    thread t1(findAllPrimes, ref(primeList), ref(ticketArray), 2);
-    thread t2(findAllPrimes, ref(primeList), ref(ticketArray), 3);
-    thread t3(findAllPrimes, ref(primeList), ref(ticketArray), 5);
-    thread t4(findAllPrimes, ref(primeList), ref(ticketArray), 7);
-    thread t5(findAllPrimes, ref(primeList), ref(ticketArray), 11);
-    thread t6(findAllPrimes, ref(primeList), ref(ticketArray), 13);
-    thread t7(findAllPrimes, ref(primeList), ref(ticketArray), 17);
-    thread t8(findAllPrimes, ref(primeList), ref(ticketArray), 19);
+    thread t1(findAllPrimes, ref(primeList), ref(ticketArray), 3);
+    thread t2(findAllPrimes, ref(primeList), ref(ticketArray), 5);
+    thread t3(findAllPrimes, ref(primeList), ref(ticketArray), 7);
+    thread t4(findAllPrimes, ref(primeList), ref(ticketArray), 11);
+    //thread t5(findAllPrimes, ref(primeList), ref(ticketArray), 13);
+    //thread t6(findAllPrimes, ref(primeList), ref(ticketArray), 17);
+    //thread t7(findAllPrimes, ref(primeList), ref(ticketArray), 19);
+    //thread t8(findAllPrimes, ref(primeList), ref(ticketArray), 23);
 
     t1.join();
     t2.join();
     t3.join();
     t4.join();
-    t5.join();
-    t6.join();
-    t7.join();
-    t8.join();
+    //t5.join();
+    //t6.join();
+    //t7.join();
+    //t8.join();
 
     auto stop = high_resolution_clock::now();
     auto duration = duration_cast<microseconds>(stop - start);
 
     
-    long long sum = 0;
-    long long numPrimes = 0;
+   
 
-    for (long long i = 0; i < upperBound; i++) {
+    for (long long i = 3; i < upperBound; i+=2) {
         if (primeList[i] == true) {
             sum += i;
             numPrimes++;
@@ -73,6 +75,7 @@ int main() {
 }
 
 void checkNonPrimes(long long start, vector <bool> &primeList) {
+      
     for (long long i = start * start; i < upperBound; i += start) {
         if (primeList[i] == false) {
             continue;
@@ -83,29 +86,32 @@ void checkNonPrimes(long long start, vector <bool> &primeList) {
     }
 }
 
-int getNextNumber(vector <bool> &ticketArray) {
-    for (int i = 2; i < sqrtOfUpperBound; i++) {
+int getNextNumber(vector <bool> &ticketArray, int start, vector <bool> &primeList) {
+    for (int i = start; i < sqrtOfUpperBound; i++) {
+        
         if (ticketArray[i] == false) {
             mtx.lock();
-            if (ticketArray[i] == true) {
-                mtx.unlock();
-                continue;
-            }
             ticketArray[i] = true;
             mtx.unlock();
             return i;
+        } else {
+           // check if prime, if so, add i + i-1
+           if (primeList[i] == true) {
+                i = i + i - 1;
+           }
         }
     }
     return -1;
+    
 }
 
 void findAllPrimes(vector <bool> &primeList, vector <bool> &ticketArray, int start) {
     int nextNumber = start;
     while (nextNumber != -1) {
         checkNonPrimes(nextNumber, primeList);
-        nextNumber = getNextNumber(ticketArray);
+        nextNumber = getNextNumber(ticketArray, nextNumber + 1, primeList);
     }
 }
-    
+
 
     
